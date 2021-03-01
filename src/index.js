f7d08581f6e73d03f661f59d9c4f2b0db09bbfb2
SUVOJIT KAR <skar@hpe.com>
2021-03-02 01:27:59 +0530

    react-redux added

--- src/index.js
+++ src/index.js
@@ -4,9 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import reducer from './reducers';
+
+// store cration
+const Mystore = createStore(reducer);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={Mystore}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
